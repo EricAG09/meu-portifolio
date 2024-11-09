821225e0d26dcffe282d4eddf1bfa41fd9403b6f
EricAG09 <33120444+EricAG09@users.noreply.github.com>
2024-11-09 13:50:10 -0300

    criado cards de skils, instituições de ensino e projetos. Ainda em andamento com a responsividade

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,14 @@
 let btnSend = document.getElementById("submit");
 let inputName = document.getElementById("name");
 let subject = document.getElementById("subject");
-let mail = document.getElementById("mail");
 let message = document.getElementById("message");
-let responseMail = document.getElementById("response-mail");
 let textMsg = document.getElementById("text-msg");
 let form = document.getElementById("form-contact");
 
 
-
-let templateParams = {
-    subject: subject.value,
-    name: inputName.value,
-    mail: mail.value,
-    message: message.value,
-};
 btnSend.disabled = true;
 function validadeField() {
-    if (inputName.value !== "" && subject.value !== "" && mail.value !== "" && message.value !== "") {
+    if (inputName.value !== "" && subject.value !== "" && message.value !== "") {
         btnSend.disabled = false;
     }
 } validadeField();
@@ -26,29 +17,26 @@ function cleanField(field) {
 }
 inputName.onkeydown = validadeField;
 subject.onkeydown = validadeField;
-mail.onkeydown = validadeField;
 message.onkeydown = validadeField;
 
 function cleanResponse() {
     btnSend.disabled = true;
     cleanField(inputName);
     cleanField(subject);
-    cleanField(mail);
     cleanField(message);
-    responseMail.className = "d-block"
     textMsg.innerText = "";
 }
 
-function sendResponse(CssClassName, txtMessage) {
-    responseMail.className = CssClassName;
-    textMsg.innerText = txtMessage;
-    setInterval(cleanResponse, 5000);
+
+function sendWhats() {
+    const number = 85994066861;
+
+    const text = `*Olá, meu nome é:* ${inputName.value} \n*Assunto:* ${subject.value}.
+    \n*Mensagem:* ${message.value}`;
+
+    const url = `https://wa.me/${number}?text=${encodeURIComponent(text).replace(/%2A/g, '*')}`;
+
+    window.open(url, "_blank");
 }
-function sendEmail() {
-    emailjs.send("site.FernandoStadler", "template_lzaqcbs", templateParams).then(function () {
-        sendResponse("send-sucess", "O seu email foi enviado com sucesso!")
-    }, function () {
-        sendResponse("send-error", "Ocorreu um erro e seu email não foi enviado")
-    });
-}
-btnSend.onclick = sendEmail;
+
+btnSend.onclick = sendWhats;
